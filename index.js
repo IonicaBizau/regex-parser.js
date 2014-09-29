4c9d0b560b13076b10ff50d5afb5b21317f72d28
Ionică Bizău <bizauionica@yahoo.com>
2014-09-29 09:37:19 +0300

    Added correct index for flags

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ RegExpParser.parse = function(input) {
     var m = input.match(/(\/?)(.+)\1([a-z]*)/i);
 
     // Invalid flags
-    if (m[2] && !/^(?!.*?(.).*?\1)[gmixXsuUAJ]+$/.test(m[2])) {
+    if (m[3] && !/^(?!.*?(.).*?\1)[gmixXsuUAJ]+$/.test(m[3])) {
         return RegExp(input);
     }
 
